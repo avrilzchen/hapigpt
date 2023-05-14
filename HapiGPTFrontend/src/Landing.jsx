e9f2041f0a5566d9abf00303682290b1d10d34e4
Avril Chen <avril.chen@uwaterloo.ca>
2023-05-14 14:49:54 -0400

    Add files via upload
    frontend + landing page

---
+++ HapiGPTFrontend/src/Landing.jsx
@@ -0,0 +1,22 @@
+function Landing() {
+  return (
+    <main>
+      <body>
+        <section>
+          <div className="circle"></div>
+          <header>
+            <a href="#"><img src="./img/logo.png" className="logo"></img></a>
+          </header>
+          <div className="content">
+            <div className="textbox">
+              <h2>The happiness you seek is just a conversation away <br/>with<span>HapiGPT.</span></h2>
+              <a href="#root"><button classname="vvd">Try HapiGPT</button></a>
+            </div>
+          </div>
+        </section>
+      </body>
+    </main>
+  );
+}
+
+export default Landing;
---
+++ HapiGPTFrontend/src/api.js
@@ -0,0 +1,22 @@
+export const fetchResponse =  async(chat) => {
+    // console.log("chat")
+    // console.log(chat)
+    try {
+        // after depoloyment you should change the fetch URL below
+        const response = await fetch('https://hpigpt-back.vercel.app', { 
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                message: chat.map((message)=> message.message).join(" \n "),
+                hapi: chat
+            })
+        })
+
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.log(error);
+    }
+}
